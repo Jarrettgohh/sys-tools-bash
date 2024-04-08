5bbdfd7d36a88dbd967da1ccc0483c7b2cd91679
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2024-04-08 08:30:42 +0800

    # Added `bit-cloud` into `.gitignore`

--- bit-cloud/test.sh
+++
@@ -1,9 +0,0 @@
-
-# pcregrep -M "import.*\{(.|\n)+\}.*from.*\"../../../mapitin-interfaces/index\"" ./overdrive1/test.js
-
-find="import[[:space:]]+?\{([a-zA-Z]|[[:space:]]|,)+?\}[[:space:]]+?from[[:space:]]+?\"@mapitin/mapitin-library.interfaces\""
-pcregrep -M -r $find /mnt/c/Users/gohja/Desktop/mapitin-repository/sys/bit-cloud/test.js 
-
-
-# find="import(.|\n)+\{(.|\n)+\}(.|\n)+from(.|\n)+\"../../../mapitin-interfaces/index\""
-# grep -P $find /mnt/c/Users/gohja/Desktop/mapitin-repository/sys/overdrive1/test.js
